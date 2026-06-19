6af16c97185af77111bc7d6a53beddf3d47ce6f7
datavizions <sartys@posteo.de>
2026-06-19 10:23:18 +0200

    mapper.ts
    chart type setup

---
+++ src/scripts/mapper.ts
@@ -0,0 +1,8 @@
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+
+// needs to process the json files and then output data for visualizations in specific format
+
+// setup another file that defines the chart types used in the prototype (charts.ts)
+
+// test the setup and output a chart overview with survey sample data
