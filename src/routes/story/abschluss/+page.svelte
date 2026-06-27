7cac7937697342568a332e2c3a52ef442c338368
datavizions <sartys@posteo.de>
2026-06-27 10:36:26 +0200

    regenerated styles variables and set up filter options
    runes path added to config file

--- src/routes/story/abschluss/+page.svelte
+++ src/routes/story/abschluss/+page.svelte
@@ -4,5 +4,6 @@
 
     const f2A1 = charts.wahrnehmung.items.find(item => item.label === 'F2A1')?.percent ?? 0;
     const percentPublic = (charts.vertrauenbetreiber.items.find(item => item.label === '1')?.percent ?? 0) + (charts.vertrauenbetreiber.items.find(item => item.label === '2')?.percent ?? 0);
+    const percentSicherheit = charts.kamerabedeutung.items.find(item => item.answer === '1')?.percent ?? 0;
 </script>
 
